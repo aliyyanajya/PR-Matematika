a7424cf286cadacc53dc2b4ea2d73ace8a61668c
aliya ruby <aliya.aurelia@gmail>
2019-02-05 10:38:31 +0700

    menambah file soal nomer 2

---
+++ soalnomer2.rb
@@ -0,0 +1,8 @@
+# Lingkaran 1
+R1 = 10    # Jari-jari lingkaran 1
+# Lingkaran 2
+R2 = 5 # Jari-jari lingkaran 2
+# Jarak atar dua titik pusat Lingkaran 1 dan Lingkaran 2
+JarakAntarTitikPusatLingkaran = 25
+# Garis singgung p;ersekutuan Dalam
+#GSPD  # Berapa jarak Garis singgung persekutuan dalam
