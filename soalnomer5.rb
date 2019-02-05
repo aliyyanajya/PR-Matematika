b47cb3b53f6702ecf1814b48f502f84c1feb927c
aliya ruby <aliya.aurelia@gmail>
2019-02-05 12:33:23 +0700

    menyelesaikan soal

--- soalnomer5.rb
+++ soalnomer5.rb
@@ -1,7 +1,21 @@
+###############################################################################
+#  Tugas lingkaran sebagai prasyarat ulangan
+#  Mapel Matematika
+#
+#  Soal Nomer 5
+#  maka panjang garis singgung persekutuan luar kedua lingkaran tersebut adalah …
+#
+# Author: Aliyya N. Aurelia
+#################################################################################Lingkaran1
 #Lingkaran1
 R1 = 7
 #Lingkaran2
 R2 = 2
-JarakAntarTitikPusatLingkaran￼ = 13
+#Jarak antar titik pusat lingkaran
+JAPL = 13
 # maka panjang garis singgung persekutuan luar kedua lingkaran tersebut adalah …
 #GSPL
+SelisihR1R2 = R1 - R2
+GSPLqrt = JAPL**2 - SelisihR1R2**2
+GSPL = Math.sqrt(GSPLqrt)
+puts "maka panjang garis singgung persekutuan luar kedua lingkaran tersebut adalah #{GSPL} cm"
