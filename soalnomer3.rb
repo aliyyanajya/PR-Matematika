643ef067d2aba836e27471611e2b9c1c34e0cd9a
aliya ruby <aliya.aurelia@gmail>
2019-02-05 11:28:09 +0700

    menyelesaikan soal

--- soalnomer3.rb
+++ soalnomer3.rb
@@ -3,3 +3,9 @@ JarakPohonCemara = 1
 HargaSatuPohon = 25000
 #Berapa biaya menanam seluruh pohoh cemara ?
 #BiayaMenanamPohonCemara
+#Keliling Lingkaran
+Pi = 3.14
+KelilingLingkaran = 2 * Pi * JariJariLingkaran
+puts "jadi keliling lingkaran adalah #{KelilingLingkaran} meter "
+BiayaMenanamPohonCemara = HargaSatuPohon * 220
+puts "Biaya menanam seluruh pohon cemara di taman adalah 220 meter * 25000 = #{BiayaMenanamPohonCemara}"
