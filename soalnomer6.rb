04e9e31bf687b136fe762f70963f1eb32b85c859
aliya ruby <aliya.aurelia@gmail>
2019-02-05 12:52:29 +0700

    menyelesaikan soal

--- soalnomer6.rb
+++ soalnomer6.rb
@@ -1,3 +1,12 @@
+###############################################################################
+#  Tugas lingkaran sebagai prasyarat ulangan
+#  Mapel Matematika
+#
+#  Soal Nomer 6
+#  panjang garis singgung persekutuan luar kedua lingkaran tersebut adalah
+#
+# Author: Aliyya N. Aurelia
+#################################################################################
 #Lingkaran1
 R1 = 26
 #Lingkaran2
@@ -5,3 +14,8 @@ R2 = 16
 JarakAntarTitikPusatLingkaran = 26
 #panjang garis singgung persekutuan luar kedua lingkaran tersebut adalah
 #GSPL
+SelisihR1R2 = R1 - R2
+GSPLqrt = R1**2 -SelisihR1R2**2
+GSPL = Math.sqrt(GSPLqrt)
+#puts GSPL
+puts "panjang garis singgung persekutuan luar kedua lingkaran tersebut adalah #{GSPL} cm"
