586024fd59e1fdf908ade44a99df68dbdac200f9
aliya ruby <aliya.aurelia@gmail>
2019-02-05 12:00:45 +0700

    menyelesaikan soal

--- soalnomer4.rb
+++ soalnomer4.rb
@@ -3,3 +3,8 @@ JarakAntarTitikPusatLingkaran = 17
 R1 = 10
 # maka panjang jari-jari lingkaran yang lain adalah ...
 # R2
+R1R2qrt = JarakAntarTitikPusatLingkaran**2 - GSPD**2
+R1R2 = Math.sqrt(R1R2qrt)
+puts "Jarak dua jari jari lingkaran adalah #{R1R2}"
+R2 = R1R2 - R1
+puts "Maka pangjang jari jari lingkaran yang lain adalah #{R2}"
