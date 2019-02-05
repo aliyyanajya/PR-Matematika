e52935b8f419371f481bb5a7de9a99501ac34bb0
aliya ruby <aliya.aurelia@gmail>
2019-02-05 10:49:57 +0700

    menyelesaikan soal

--- soalnomer2.rb
+++ soalnomer2.rb
@@ -1,3 +1,13 @@
+###############################################################################
+#  Tugas lingkaran sebagai prasyarat ulangan
+#  Mapel Matematika
+#
+#  Soal Nomer 2
+#  Mencari nilai Jarak Garis singgung persekutuan dalam
+#  diantara 2 Lingkaran
+#
+# Author: Aliyya N. Aurelia
+################################################################################
 # Lingkaran 1
 R1 = 10    # Jari-jari lingkaran 1
 # Lingkaran 2
@@ -6,3 +16,7 @@ R2 = 5 # Jari-jari lingkaran 2
 JarakAntarTitikPusatLingkaran = 25
 # Garis singgung p;ersekutuan Dalam
 #GSPD  # Berapa jarak Garis singgung persekutuan dalam
+
+GSPDqrt = JarakAntarTitikPusatLingkaran**2 - (R1+R2)**2
+GSPD = Math.sqrt(GSPDqrt)
+puts "Jadi Garis singgung persekutuan dalam adalah #{GSPD} cm"
